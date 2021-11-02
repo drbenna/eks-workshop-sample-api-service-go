e3dd3bac643af598a8435d657240de402ce1bdf9
drbenna <91122449+drbenna@users.noreply.github.com>
2021-11-02 15:14:12 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Cohort 8 <br> and Munish"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
